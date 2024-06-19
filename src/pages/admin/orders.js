9e909d614559ab9d20b01e591f65dabba046184d
jastute <centricloset@gmail.com>
2024-06-19 09:44:29 +0300

    profile page updated

--- src/pages/admin/orders.js
+++ src/pages/admin/orders.js
@@ -1,7 +1,176 @@
+// import React, { useState, useEffect } from 'react';
+// import toast, {Toaster} from 'react-hot-toast'; 
+// import { TiTick } from 'react-icons/ti';
+// import { RxCrossCircled } from "react-icons/rx"; 
+
+// const statusClasses = {
+//   accepted: 'bg-green-100 text-green-800',
+//   pending: 'bg-yellow-100 text-yellow-800',
+//   rejected: 'bg-red-100 text-red-800',
+// };
+
+// const Orders = () => {
+//   const [filter, setFilter] = useState('All');
+//   const [orders, setOrders] = useState([]);
+//   const [loading, setLoading] = useState(true);
+
+//    useEffect(() => {
+//     const fetchOrders = async () => {
+//       try {
+//         const response = await fetch('https://medrent-server.vercel.app/api/bookings');
+//         const data = await response.json();
+//         if (data.message === 'success') {
+//           const sortedOrders = data.bookings
+//             .map(booking => ({
+//               id: booking._id,
+//               createdAt: booking.createdAt,
+//               customer: booking.user?.fullName,
+//               equipment: booking.inventory?.title,
+//               rentingPeriod: `${booking.rentalPeriod} weeks`,
+//               phoneNumber: booking.user?.phone,
+//               status: booking.status,
+//               pickUpDate: new Date(booking.startDate).toLocaleDateString(),
+//             }))
+//             .sort((a, b) => {
+//               if (a.status === 'pending') return -1;
+//               if (b.status === 'pending') return 1;
+//               return 0;
+//             });
+//           setOrders(sortedOrders);
+//         } else {
+//           console.error('Failed to fetch orders');
+//         }
+//       } catch (error) {
+//         console.error('Error fetching orders:', error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchOrders();
+//   }, []);
+//   // console.log(orders);
+
+//   const filteredOrders = filter === 'All' ? orders : orders.filter(order => order.status === filter);
+
+  
+// // change booking status
+//   const handleChangeStatus = async (orderId, newStatus) => {
+//     try {
+//         const response = await fetch(`https://medrent-server.vercel.app/api/bookings/${orderId}`, {
+//             method: 'PUT',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify({ status: newStatus }),
+//         });
+
+//         const data = await response.json();
+//         if (response.ok) {
+//           setOrders(prevOrders => prevOrders.map(order => {
+//             if (order.id === orderId) {
+//               return { ...order, status: newStatus };
+//             }
+//             return order;
+//           }));
+//           toast.success(`Booking status updated successfully!`,{
+//             duration: 3000,
+//             position: 'top-center', 
+//           });
+//         } else throw new Error(data.message || 'Failed to update booking status');
+//     } catch (error) {
+//         console.error('Error updating booking status:', error);
+//         toast.error(`Failed to update booking status!`,{
+//           duration: 3000,
+//           position: 'top-center',
+//         });
+//     }
+// };
+
+
+//   if (loading) {
+//     return <div className='flex justify-center items-center min-h-30vh'>Loading...</div>;
+//   }
+
+//   return (
+//     <div className="min-h-screen p-4">
+//       <h1 className="text-2xl font-semibold mb-4 text-center">Bookings</h1>
+
+//       <div className="mb-4">
+//         <label htmlFor="statusFilter" className="mr-2">Filter by Status:</label>
+//         <select
+//           id="statusFilter"
+//           className="p-2 border rounded"
+//           value={filter}
+//           onChange={(e) => setFilter(e.target.value)}
+//         >
+//           <option value="All">All</option>
+//           <option value="pending">Pending</option>
+//           <option value="accepted">Accepted</option>
+//           <option value="rejected">Rejected</option>
+//         </select>
+//       </div>
+
+//       <div className="overflow-x-auto">
+//         <table className="min-w-full bg-white border">
+//           <thead>
+//             <tr className='bg-slate-200'>
+//               <th className="py-2 px-4 border-b text-left">Customer</th>
+//               <th className="py-2 px-4 border-b text-left">Equipment</th>
+//               <th className="py-2 px-4 border-b text-left">Renting Period</th>
+//               <th className="py-2 px-4 border-b text-left">Phone Number</th>
+//               <th className="py-2 px-4 border-b text-left">Status</th>
+//               <th className="py-2 px-4 border-b text-left">Pick Up Date</th>
+//               <th className="py-2 px-4 border-b text-left">Actions</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {filteredOrders.map(order => (
+//               <tr key={order.id}>
+//                 <td className="py-2 px-4 border-b">{order.customer}</td>
+//                 <td className="py-2 px-4 border-b">{order.equipment}</td>
+//                 <td className="py-2 px-4 border-b">{order.rentingPeriod}</td>
+//                 <td className="py-2 px-4 border-b">{order.phoneNumber}</td>
+//                 <td className="py-2 px-4 border-b">
+//                   <span className={`px-2 py-1 rounded-full text-xs font-semibold ${statusClasses[order.status]}`}>
+//                     {order.status}
+//                   </span>
+//                 </td>
+//                 <td className="py-2 px-4 border-b">{order.pickUpDate}</td>
+//                 <td className="py-2 px-4 border-b ">
+//                   {order.status === 'pending' && (
+//                     <div className='flex justify-center items-center '>
+//                       <button onClick={() => handleChangeStatus(order.id, 'accepted')} className="px-2 py-1 bg-green-500 text-white rounded mr-2">Accept</button>
+//                       <button onClick={() => handleChangeStatus(order.id, 'rejected')} className="px-2 py-1 bg-red-500 text-white rounded">Reject</button>
+//                     </div>
+//                   )}
+//                   {order.status === 'accepted' && (
+//                     <div className='flex justify-center'>
+//                       <button className="px-12 py-1 bg-green-100 cursor-not-allowed text-white rounded mr-2 disabled "> <TiTick color='green' size={20} /></button>
+//                     </div>
+//                   )}
+//                   {order.status === 'rejected' && (
+//                     <div className='flex justify-center'>
+//                       <button className="px-12 py-1 bg-red-100 cursor-not-allowed text-white rounded mr-2 disabled"><RxCrossCircled size={20} color='red'/></button>
+//                     </div>
+//                   )}
+                
+//                 </td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       </div>
+//       <Toaster/>
+//     </div>
+//   );
+// };
+
+// export default Orders;
 import React, { useState, useEffect } from 'react';
-import toast, {Toaster} from 'react-hot-toast'; 
+import toast, { Toaster } from 'react-hot-toast';
 import { TiTick } from 'react-icons/ti';
-import { RxCrossCircled } from "react-icons/rx"; 
+import { RxCrossCircled } from 'react-icons/rx';
 
 const statusClasses = {
   accepted: 'bg-green-100 text-green-800',
@@ -14,7 +183,7 @@ const Orders = () => {
   const [orders, setOrders] = useState([]);
   const [loading, setLoading] = useState(true);
 
-   useEffect(() => {
+  useEffect(() => {
     const fetchOrders = async () => {
       try {
         const response = await fetch('https://medrent-server.vercel.app/api/bookings');
@@ -53,40 +222,47 @@ const Orders = () => {
 
   const filteredOrders = filter === 'All' ? orders : orders.filter(order => order.status === filter);
 
-  
-// change booking status
+  // Change booking status
   const handleChangeStatus = async (orderId, newStatus) => {
     try {
-        const response = await fetch(`https://medrent-server.vercel.app/api/bookings/${orderId}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ status: newStatus }),
-        });
+      const response = await fetch(`https://medrent-server.vercel.app/api/bookings/${orderId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ status: newStatus }),
+      });
 
-        const data = await response.json();
-        if (response.ok) {
-          setOrders(prevOrders => prevOrders.map(order => {
+      const data = await response.json();
+      if (response.ok) {
+        setOrders(prevOrders => {
+          const updatedOrders = prevOrders.map(order => {
             if (order.id === orderId) {
               return { ...order, status: newStatus };
             }
             return order;
-          }));
-          toast.success(`Booking status updated successfully!`,{
-            duration: 3000,
-            position: 'top-center', 
           });
-        } else throw new Error(data.message || 'Failed to update booking status');
-    } catch (error) {
-        console.error('Error updating booking status:', error);
-        toast.error(`Failed to update booking status!`,{
+
+          // Sort the orders so that pending ones stay at the top
+          return updatedOrders.sort((a, b) => {
+            if (a.status === 'pending') return -1;
+            if (b.status === 'pending') return 1;
+            return 0;
+          });
+        });
+        toast.success(`Booking status updated successfully!`, {
           duration: 3000,
           position: 'top-center',
         });
+      } else throw new Error(data.message || 'Failed to update booking status');
+    } catch (error) {
+      console.error('Error updating booking status:', error);
+      toast.error(`Failed to update booking status!`, {
+        duration: 3000,
+        position: 'top-center',
+      });
     }
-};
-
+  };
 
   if (loading) {
     return <div className='flex justify-center items-center min-h-30vh'>Loading...</div>;
@@ -154,14 +330,14 @@ const Orders = () => {
                       <button className="px-12 py-1 bg-red-100 cursor-not-allowed text-white rounded mr-2 disabled"><RxCrossCircled size={20} color='red'/></button>
                     </div>
                   )}
-                
+
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
-      <Toaster/>
+      <Toaster />
     </div>
   );
 };
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,13 +1,167 @@
-import React from 'react';
+// import React from 'react';
+// import { NavLink } from 'react-router-dom';
+// import { useState, useEffect } from 'react';
+
+// const UserProfile = () => {
+
+
+//     const [user, setUserData] = useState({});
+//     const [loading, setLoading] = useState(false);
+//     // const [bookings, setBooking]=useState([])
+
+//   useEffect(() => {
+//     try {
+//       const userData = sessionStorage.getItem('user');
+//       if (userData) {
+//         setUserData(JSON.parse(userData));
+//       }
+//       setLoading(false);
+//     } catch (error) {
+//       console.error('Error fetching user data:', error);
+//       setLoading(false);
+//     }
+//   }, [loading]);
+
+
+
+// //   fetch your orders
+// const [bookings, setBooking] = useState([]);
+// const [fetching, setFetching] = useState(true);
+// // console.log(bookings);
+
+
+// useEffect(() => {
+//   const fetchOrders = async () => {
+//     try {
+//       const response = await fetch(`https://medrent-server.vercel.app/api/bookings/${user?.userID}`);
+//       const data = await response.json();
+//       // console.log(data);
+//       if (data.message === 'success') {
+//         setBooking(data.bookings.map(booking => {
+//           // convert to integer
+//           const rentalPeriodWeeks = parseFloat(booking.rentalPeriod);
+//           const rentalPeriodDays = rentalPeriodWeeks * 7;
+//           const pricePerDay = parseFloat(booking.inventory?.price);
+//           const totalPrice = pricePerDay * rentalPeriodDays;
+//           return {
+//             id: booking._id,
+//             customer: booking.user?.fullName, 
+//             equipment: booking.inventory?.title, 
+//             price: booking.inventory?.price, 
+//             rentingPeriod: `${rentalPeriodWeeks} weeks`, 
+//             phoneNumber: booking.user?.phone, 
+//             status: booking.status,
+//             pickUpDate: new Date(booking.startDate).toLocaleDateString(),
+//             totalPrice
+//           };
+//         }));
+//       } else {
+//         console.error('Failed to fetch orders');
+//       }
+//     } catch (error) {
+//       console.error('Error fetching orders:', error);
+//     } finally {
+//         setFetching(false);
+//     }
+//   };
+
+//   fetchOrders();
+// }, [user?.userID]);
+
+  
+
+//   if (loading) {
+//     return <div className='flex items-center justify-center min-h-[70vh]'>Loading...</div>;
+//   }
+
+
+//   return (
+//     <div className="bg-white rounded-lg shadow-md p-6 min-h-[70vh] font-poppins">
+//       <div className="flex items-center mb-4 justify-center">
+//         <div className="mr-4">
+//           <img
+//             src="https://via.placeholder.com/64"
+//             alt="User Avatar"
+//             className="min-w-16 min-h-16 rounded-full"
+//           />
+//         </div>
+//         <div>
+//           <h2 className="text-xl font-semibold">{user?.fullName}</h2>
+//           <p className="text-gray-600">{user?.email}</p>
+//           <p className="text-gray-600">{user?.organization}</p>
+//           <p className="text-gray-600">{user?.phone}</p>
+//           <p className="text-gray-600"><b>User ID:</b> {user?.userID}</p>
+//         </div>
+//       </div>
+
+//       <div className=' mx-4 md:mx-32 lg:mx-40'>
+//         <div>
+//         <h3 className="text-lg font-semibold mb-2">Bookings</h3>
+//         {bookings.length > 0 ? (
+//           <div className="flex flex-wrap gap-4">
+//           {bookings.map((booking) => (
+//             <div key={booking.id} className="max-w-sm bg-white rounded-lg shadow-md overflow-hidden min-w-64 w-72">
+//               <div className="bg-[#111827] text-white p-4 flex items-center justify-between">
+//                 <span className="flex items-center space-x-2">
+//                   <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+//                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+//                   </svg>
+//                   <span>{booking.status}</span>
+//                 </span>
+//                 <span>{booking.equipment}</span>
+//               </div>
+//               <div className="p-4">
+//                 <div className="mb-2">
+//                   <span className="font-semibold">Rental Period:</span> {booking.rentingPeriod}
+//                 </div>
+//                 <div className="mb-2">
+//                   <span className="font-semibold">Price per Day:</span> Ksh.{booking.price}
+//                 </div>
+//                 <div className="mb-2">
+//                   <span className="font-semibold">Total Price:</span> Ksh.{booking.totalPrice.toFixed(2)}
+//                 </div>
+//                 <div className="mb-2">
+//                   <span className="font-semibold">Pick-up Date:</span> {booking.pickUpDate}
+//                 </div>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//         ) : fetching ? (
+//           <div className="text-gray-600 min-h-[20vh] mt-4">loading...</div>
+//         ):(
+//           <div>
+//             <div className="text-gray-600">No bookings found.</div>
+//             <div className="mt-4">
+//             <NavLink
+//               to="/equipmemts"
+//               className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+//             >
+//               Rent Equipment
+//             </NavLink>
+//           </div>
+//           </div>
+//         )}
+
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default UserProfile;
+
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { TiTick } from 'react-icons/ti';
+import { RxCrossCircled } from "react-icons/rx";
+import { GrInProgress } from "react-icons/gr";
 
 const UserProfile = () => {
-
-
-    const [user, setUserData] = useState({});
-    const [loading, setLoading] = useState(false);
-    // const [bookings, setBooking]=useState([])
+  const [user, setUserData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [bookings, setBooking] = useState([]);
+  const [fetching, setFetching] = useState(true);
 
   useEffect(() => {
     try {
@@ -22,107 +176,124 @@ const UserProfile = () => {
     }
   }, [loading]);
 
-
-
-//   fetch your orders
-const [bookings, setBooking] = useState([]);
-const [fetching, setFetching] = useState(true);
-// console.log(bookings);
-
-
-useEffect(() => {
-  const fetchOrders = async () => {
-    try {
-      const response = await fetch(`https://medrent-server.vercel.app/api/bookings/${user?.userID}`);
-      const data = await response.json();
-      console.log(data);
-      if (data.message === 'success') {
-        setBooking(data.bookings.map(booking => ({
-          id: booking._id,
-          customer: booking.user?.fullName, 
-          equipment: booking.inventory?.title, 
-          price: booking.inventory?.price, 
-          rentingPeriod: `${booking.rentalPeriod} weeks`, 
-          phoneNumber: booking.user?.phone, 
-          status: booking.status,
-          pickUpDate: new Date(booking.startDate).toLocaleDateString(),
-          
-        })));
-      } else {
-        console.error('Failed to fetch orders');
-      }
-    } catch (error) {
-      console.error('Error fetching orders:', error);
-    } finally {
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const response = await fetch(`https://medrent-server.vercel.app/api/bookings/${user?.userID}`);
+        const data = await response.json();
+        if (data.message === 'success') {
+          setBooking(data.bookings.map(booking => {
+            const rentalPeriodWeeks = parseFloat(booking.rentalPeriod);
+            const rentalPeriodDays = rentalPeriodWeeks * 7;
+            const pricePerDay = parseFloat(booking.inventory?.price);
+            const totalPrice = pricePerDay * rentalPeriodDays;
+            return {
+              id: booking._id,
+              customer: booking.user?.fullName,
+              equipment: booking.inventory?.title,
+              price: booking.inventory?.price,
+              rentingPeriod: `${rentalPeriodWeeks} weeks`,
+              phoneNumber: booking.user?.phone,
+              status: booking.status,
+              pickUpDate: new Date(booking.startDate).toLocaleDateString(),
+              totalPrice
+            };
+          }));
+        } else {
+          console.error('Failed to fetch orders');
+        }
+      } catch (error) {
+        console.error('Error fetching orders:', error);
+      } finally {
         setFetching(false);
-    }
-  };
+      }
+    };
 
-  fetchOrders();
-}, [user?.userID]);
-
-  
+    fetchOrders();
+  }, [user?.userID]);
 
   if (loading) {
     return <div className='flex items-center justify-center min-h-[70vh]'>Loading...</div>;
   }
 
-
   return (
     <div className="bg-white rounded-lg shadow-md p-6 min-h-[70vh] font-poppins">
-      <div className="flex items-center mb-4 justify-center">
-        <div className="mr-4">
-          <img
-            src="https://via.placeholder.com/64"
-            alt="User Avatar"
-            className="w-16 h-16 rounded-full"
-          />
+      <div className="flex flex-col md:flex-row justify-around mb-4">
+        <div className="flex items-center mb-4 md:mb-0">
+          <div className="mr-4">
+            <img
+              src="https://via.placeholder.com/64"
+              alt="User Avatar"
+              className="min-w-16 min-h-16 rounded-full"
+            />
+          </div>
+          <div>
+            <h2 className="text-xl font-semibold">{user?.fullName}</h2>
+            <p className="text-gray-600">{user?.email}</p>
+            <p className="text-gray-600">{user?.organization}</p>
+            <p className="text-gray-600">{user?.phone}</p>
+            <p className="text-gray-600"><b>User ID:</b> {user?.userID}</p>
+          </div>
         </div>
-        <div>
-          <h2 className="text-xl font-semibold">{user?.fullName}</h2>
-          <p className="text-gray-600">{user?.email}</p>
-          <p className="text-gray-600">{user?.organization}</p>
-          <p className="text-gray-600">{user?.phone}</p>
-          <p className="text-gray-600"><b>User ID:</b> {user?.userID}</p>
-        </div>
+        {bookings.length > 0 && <div className="  md:w-1/3 md:border-l-2 md:pl-12 mx-2 mt-7">
+          <h3 className="text-lg font-semibold mb-2">Payment Guide</h3>
+          <p className="text-gray-600 mb-2"><b>N/B:</b> Half of the payment should be done upon delivery while the rest will be made as booking period proceed or at the end:</p>
+          <ul className="text-gray-600">
+            <li><span className='font-semibold'>M-Pesa </span>(Send to 0721349587)</li>
+            <li> <span className='font-semibold'>Bank Transfer </span> (Equity Account No: 987654321)</li>
+            <li><span className='font-semibold'>Paybill </span> (Business No: 123456, Account No: MEDRENT)</li>
+          </ul>
+        </div>}
       </div>
 
-      <div className=' mx-4 md:mx-32'>
-        <div>
+      <div className=' mx-2 md:mx-32 lg:mx-40'>
         <h3 className="text-lg font-semibold mb-2">Bookings</h3>
         {bookings.length > 0 ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          <div className="flex flex-wrap gap-4">
             {bookings.map((booking) => (
-              <div
-                key={booking.id}
-                className="bg-gray-100 rounded-lg p-4 shadow-sm"
-              >
-                <p className="text-gray-600"><b>Equipment:</b> {booking.equipment}</p>
-                <p className="text-gray-600">
-                  <b>Rental Period:</b> {booking.rentingPeriod}
-                </p>
-                <p className="text-gray-600 py-1"><b>Status:</b> <span className={`${booking.status==='pending'? "bg-yellow-100 text-yellow-800": booking.status==='accepted'? "bg-green-100 text-green-800":"bg-red-100 text-red-800"} rounded-md px-2 py-0.5`}>{booking.status}</span></p>
-                <p className="text-gray-600">
-                  <b>Pick Up Date:</b> {booking.pickUpDate}
-                </p>
+              <div key={booking.id} className="max-w-sm bg-white rounded-lg shadow-md overflow-hidden min-w-64 w-72">
+                <div className="bg-[#111827] text-white p-4 flex items-center justify-between">
+                  <span className="flex items-center space-x-2">
+                    {/* <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+                    </svg> */}
+                    {booking.status === 'pending' ? <GrInProgress className="w-5 h-5 text-yellow-500" />: booking.status === 'accepted' ? <TiTick className="w-6 h-6 text-green-500" /> : <RxCrossCircled className="w-6 h-6 text-red-500" />}
+                    <span>{booking.status}</span>
+                  </span>
+                  <span>{booking.equipment}</span>
+                </div>
+                <div className="p-4">
+                  <div className="mb-2">
+                    <span className="font-semibold">Rental Period:</span> {booking.rentingPeriod}
+                  </div>
+                  <div className="mb-2">
+                    <span className="font-semibold">Price per Day:</span> Ksh.{booking.price}
+                  </div>
+                  <div className="mb-2">
+                    <span className="font-semibold">Total Price:</span> Ksh.{booking.totalPrice.toFixed(2)}
+                  </div>
+                  <div className="mb-2">
+                    <span className="font-semibold">Pick-up Date:</span> {booking.pickUpDate}
+                  </div>
+                </div>
               </div>
             ))}
           </div>
+        ) : fetching ? (
+          <div className="text-gray-600 min-h-[20vh] mt-4">loading...</div>
         ) : (
-          <div className="text-gray-600">No bookings found.</div>
-        )}
-
-        {bookings.length === 0 && (
-          <div className="mt-4">
-            <NavLink
-              to="/equipmemts"
-              className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >
-              Rent Equipment
-            </NavLink>
+          <div>
+            <div className="text-gray-600">No bookings found.</div>
+            <div className="mt-4">
+              <NavLink
+                to="/equipments"
+                className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              >
+                Rent Equipment
+              </NavLink>
+            </div>
           </div>
         )}
-        </div>
       </div>
     </div>
   );
